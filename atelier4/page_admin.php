62ec94c17f31c711fb1bb7bf20df3f1dc738cb91
catia91100 <catia.catarino@yahoo.fr>
2024-12-20 14:04:15 +0100

    Update page_admin.php

--- atelier4/page_admin.php
+++ atelier4/page_admin.php
@@ -1,3 +1,10 @@
 <?php
 // Démarrer la session
 session_start();
+
+if ($_SERVER['PHP_AUTH_USER'] == $valid_username ='admin' || $_SERVER['PHP_AUTH_PW'] == $valid_password ='secret' ) {
+    // Si les identifiants sont corrects
+    header('WWW-Authenticate: Basic realm="Zone Protégée"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Nom d\'utilisateur ou mot de passe correct.';
+    exit;
