18a81ec81dc194f25c092e6e4ebabea5691f1935
catia91100 <catia.catarino@yahoo.fr>
2024-12-20 11:13:34 +0100

    Update page_admin.php

--- atelier3/page_admin.php
+++ atelier3/page_admin.php
@@ -6,6 +6,10 @@ session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['username']==='admin') {
     header('Location: page_admin.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
     exit();
+    }
+else (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['username']==='user') {
+    header('Location: page_user.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
+    exit();
 }
 ?>
 
