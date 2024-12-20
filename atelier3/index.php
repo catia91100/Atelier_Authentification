83233da14978d3dc2a4b572ed611b53a4d617077
catia91100 <catia.catarino@yahoo.fr>
2024-12-20 10:41:54 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -3,11 +3,11 @@
 session_start();
 
 // Vérifier si l'utilisateur est déjà connecté
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['admin']===$username) {
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['username']===$admin) {
     header('Location: page_admin.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
     exit();
 }
-    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['user']===$username) {
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['username']===$user) {
     header('Location: page_user.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
     exit();
 }
