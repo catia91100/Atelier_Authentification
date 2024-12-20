dd631a0031b70a7061c077061ee314e6142a3e3b
catia91100 <catia.catarino@yahoo.fr>
2024-12-20 11:42:18 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -13,12 +13,13 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['
 }
 
 //initialisation de ma variable
-if(!isset ($_SESSION['counter']) )$_SESSION['counter']  = "";
+if(!isset ($_SESSION['counter']) )$_SESSION['counter']  = "";{
 $_SESSION['counter'] = 0;
 // incrémenter le compteur
 $_SESSION['counter']++;
 // montrer le résultat à l'écran
 echo "Vous avez vu " . $_SESSION['counter'] . " fois cette page";
+}
 ?>
 
 // Gérer le formulaire de connexion
